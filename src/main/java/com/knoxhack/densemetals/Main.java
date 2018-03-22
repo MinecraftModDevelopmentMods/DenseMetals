83510b69374aab80fe73c7bebf06e738c49c3f42
knoxhack <hackofgod@gmail.com>
2018-03-22 16:15:19 -0400

    fixes

--- src/main/java/com/knoxhack/densemetals/Main.java
+++ src/main/java/com/knoxhack/densemetals/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
     public static final String MODID = "densemetals";
     public static final String MODNAME = "Dense Metals";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.1.0";
 	public static final CreativeTabMain creativeTab = new CreativeTabMain();
 
     @SidedProxy(clientSide = "com.knoxhack.densemetals.proxy.ClientProxy", serverSide = "com.knoxhack.densemetals.proxy.ServerProxy")
